6d6454fbf24482e6918d9f5551ccef64aecea2b2
aladeamidat1 <aladeamidatayomide@gmail.com>
2026-02-10 19:35:49 +0100

    upload filed push

--- tests/createEventPrivate.spec.ts
+++ tests/createEventPrivate.spec.ts
@@ -14,7 +14,24 @@ test.describe('Create Event', () => {
         await loginPage.login('noreply-dev+yes@enumverse.com', 'Sunday@222');
         await loginPage.loginButton.click();
 
-        await page.waitForURL = ("https://educator.dev.enumverse.com/admin/dashboard");
-        
+        await page.waitForURL("https://educator.dev.enumverse.com/admin/dashboard");
+
     });
+
+    test('should create a private event successfully', async ({ page }) => {
+        const createEventPage = new CreateEventPage(page);
+        await createEventPage.goto();
+        await createEventPage.openCalender();
+        await createEventPage.openCreateEvent();
+        await createEventPage.enterEventName('Realtors Networking');
+        await createEventPage.selectEventVisibilityPrivate();
+        await createEventPage.enterEventDescription('A networking event for realtors to connect and share insights.');
+        await createEventPage.enterEventStartDate('2023-09-20');
+        await createEventPage.enterEventStartTime('10:00 AM');
+        await createEventPage.enterEventEndDate('2023-09-21');
+        await createEventPage.enterEventEndTime('11:00 AM');
+        await createEventPage.enterEventLocation('Online');
+        await createEventPage.submit();
+    });
+    
 });
---
+++ tests/create_event_public.spec.ts
@@ -0,0 +1,38 @@
+import{test, expect} from '@playwright/test';
+import { CreateEventPage } from '../pages/creatEventPage';
+import { OrgLoginPage } from '../pages/OrgLoginPage';   
+
+
+test.describe('Create Event', () => {
+    test.beforeEach(async ({ page }) => {
+        const 
+        loginPage = new OrgLoginPage(page);
+        await loginPage.goto();
+        
+        await loginPage.openLoginDropdown();
+        await loginPage.selectOrganizationLogin();
+        await loginPage.login('noreply-dev+yes@enumverse.com', 'Sunday@222');
+        await loginPage.loginButton.click();
+
+        await page.waitForURL("https://educator.dev.enumverse.com/admin/dashboard");
+
+    });
+
+    test('should create a public event successfully', async ({ page }) => {
+        const createEventPage = new CreateEventPage(page);
+        await createEventPage.goto();
+        await createEventPage.openCalender();
+        await createEventPage.openCreateEvent();
+        await createEventPage.enterEventName('Realtors Networking');
+        await createEventPage.selectEventVisibilityPublic();
+        await createEventPage.selectEventCategory('Networking');
+        await createEventPage.enterEventDescription('A networking event for realtors to connect and share insights.');
+        await createEventPage.enterEventStartDate('2023-09-20');
+        await createEventPage.enterEventStartTime('10:00 AM');
+        await createEventPage.enterEventEndDate('2023-09-21');
+        await createEventPage.enterEventEndTime('11:00 AM');
+        await createEventPage.enterEventLocation('Online');
+        await createEventPage.submit();
+    });
+    
+});
